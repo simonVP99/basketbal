d058c0962d1fe6b1fa7ff55037c27fdaf7ab7368
Simon Van Parys <simonVP@users.noreply.huggingface.co>
2022-10-26 15:20:32 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
+from fastai.vision.all import *
 import gradio as gr
 
-def greet(name):
-    return "Hello " + name + "!!"
+def is_player(x): return x[0]
+learn = load_learner('model_basketball.pkl')
 
-iface = gr.Interface(fn=greet, inputs="text", outputs="text")
-iface.launch()
+def classify_image(img):
+    pred, idx, probs = learn.predict(img)
+    return f'{pred} with a probability of {probs[idx]:.04f}'
+    
+image = gr.inputs.Image()
+label = gr.outputs.Label()
+
+intf = gr.Interface(fn = classify_image, inputs = image, outputs = label)
+intf.launch(inline = False)
