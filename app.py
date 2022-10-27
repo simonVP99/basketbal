cc4bd41e0a704e24649275f9bdf76616037b34fc
simonVP99 <78324952+simonVP99@users.noreply.github.com>
2022-10-27 16:59:06 +0200

    inital commit

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import gradio as gr
 from fastai.vision.all import *
 import skimage
 
-learn = load_learner('export.pkl')
+learn = load_learner('model_basketball.pkl')
 
 labels = learn.dls.vocab
 def predict(img):
@@ -10,7 +10,7 @@ def predict(img):
     pred,pred_idx,probs = learn.predict(img)
     return {labels[i]: float(probs[i]) for i in range(len(labels))}
 
-title = "Pet Breed Classifier"
+title = "Basketball player classifier"
 description = "A pet breed classifier trained on the Oxford Pets dataset with fastai. Created as a demo for Gradio and HuggingFace Spaces."
 article="<p style='text-align: center'><a href='https://tmabraham.github.io/blog/gradio_hf_spaces_tutorial' target='_blank'>Blog post</a></p>"
 interpretation='default'
